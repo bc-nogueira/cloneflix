2e2698e3d348a50ea3f36e8524ea3caec1f56298
Breno Nogueira <bcnogueira@id.uff.br>
2020-08-24 23:26:02 -0300

    Change links to header icons.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,12 +5,12 @@ export default ({ black }) => {
   return (
     <header className={black ? "black" : ""}>
       <div className="header-logo">
-        <a href="http://netflix.com/">
+        <a href="https://cloneflix.netlify.app/">
           <img src={require("../../images/netflix.png")} alt="netflix-log" />
         </a>
       </div>
       <div className="header-user">
-        <a href="http://netflix.com/">
+        <a href="https://cloneflix.netlify.app/">
           <img src={require("../../images/user.png")} alt="user-logo" />
         </a>
       </div>
